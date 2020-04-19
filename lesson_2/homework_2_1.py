9e741121468ce731c6948187f47a6724107ddbfa
vibhagoel <vibhagoelbe@gmail.com>
2020-04-18 23:37:29 -0400

    First Commit - Homework Lesson 1 and 2

--- lesson_2/homework_2_1.py
+++ lesson_2/homework_2_1.py
@@ -1,33 +1,33 @@
 # Create three strings using three different methods. Save your result to result_string_1, result_string_2,
 # result_string_3 variables
 
-result_string_1 = None
-result_string_2 = None
-result_string_3 = None
+result_string_1 = 'abc'
+result_string_2 = "hello"
+result_string_3 = '''hello world'''
 
 
 # Enter a string with your name using console and save the result to result_name variable
-
-result_name = None
+full_name = input('Please enter name ')
+result_name = full_name
 
 
 # Enter your first and  last name. Join them together with a space in
 # between. Save a result in a variable result_full_name and
 # save the length of the whole name in result_full_name_length variable.
 
-first_name = None
-last_name = None
-result_full_name = None
-result_full_name_length = None
+first_name = "vibha"
+last_name = "goel"
+result_full_name = first_name +" "+ last_name
+result_full_name_length = len(result_full_name)
 
 
 # Enter the capital city of California State in lower case. Change the case to title case.
 # Save the result in result_ca_capital variable
 
-result_ca_capital = None
+result_ca_capital = 'Sacramento'.lower()
 
 
 # Enter the name of our planet. Change the case to upper case. Save the result in
 # result_planet variable
 
-result_planet = None
+result_planet = 'Earth'.upper()
--- lesson_2/homework_2_3.py
+++ lesson_2/homework_2_3.py
@@ -3,5 +3,9 @@
 # !important you should save only number to result_temperature. Formula (32°F − 32) × 5/9 = 0°C
 
 # type your code here
-temperature_fahrenheit = None
-result_temperature = None
+temperature_fahrenheit = input("enter temperature in Farenheit- ")
+temp_far = float(temperature_fahrenheit)
+temp_cel = (temp_far - 32)*5 / 9
+
+result_temperature = int(temp_cel)
+
